2764bee5f4eb032489f662f2c802a345ce158069
Jason Cauwels <troutacular@gmail.com>
2016-08-18 10:20:27 -0700

    jetpack dev mode init

---
+++ jetpack-dev.php
@@ -0,0 +1,11 @@
+<?php
+/*
+   Plugin Name: Jetpack Development Mode
+   Description: Allows you to use Jetpack in Development Mode in a local WordPress setup.
+   Author: troutacular
+   Author URI: mailto:troutacular@gmail.com
+   Version: 1.0
+ */
+
+define( 'JETPACK_DEV_DEBUG', true);
+
---
+++ readme.md
@@ -0,0 +1,3 @@
+# Jetpack Development Mode
+
+This plugin allows you to use Jetpack while in Development Mode on your local machine.
